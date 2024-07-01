18906e91cf54f8e0dd25ba878f1e160e5af846a8
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-07-01 10:43:00 -0400

    fix: specify error exit.

--- jenkins/start-jenkins.sh
+++ jenkins/start-jenkins.sh
@@ -2,7 +2,7 @@
 
 if [[ $EUID != 0 ]] ; then
     echo "You have to be root for this operation."
-    exit
+    exit 1
 fi
 
 podman-compose --file jenkins-compose.yaml up --force-recreate --detach --build --pull
