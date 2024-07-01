df78d22d4670fbd3b5f7f45b6b1b739779578398
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-07-01 10:42:11 -0400

    fix: need root to execute this script.

--- jenkins/start-jenkins.sh
+++ jenkins/start-jenkins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]] ; then
+    echo "You have to be root for this operation."
+    exit
+fi
+
 podman-compose --file jenkins-compose.yaml up --force-recreate --detach --build --pull
