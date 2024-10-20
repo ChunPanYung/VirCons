128201e99d7328d12bdde533a4d631acf55541ab
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-10-19 23:19:43 -0400

    feat: dynamically setup ssh pub key.

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -2,4 +2,10 @@
 
 # onlyoffice setup with podman and quadlet
 cp ./jenkins-pod.kube ./jenkins-pod.yaml /etc/containers/systemd/
-podman secret create jenkins-secret ./jenkins-secret.yml
+# Generate ssh key
+ssh-keygen -f ./ssh_key -t ed25519 -N ""
+
+base64_data=$(base64 --wrap=0 ./ssh_key.pub)
+sed --in-place "s/\(\s\sssh-pubkey:\s\).*$/\1${base64_data}/"
+
+podman kube play jenkins-key.yml
