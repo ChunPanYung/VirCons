f126fb3da0c5f87cbfa6138fd2bf595c6a761d84
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-10-26 22:58:42 -0400

    feat: use bash script to setup jenkins.

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -6,6 +6,18 @@ cp ./jenkins-pod.kube ./jenkins-pod.yaml /etc/containers/systemd/
 ssh-keygen -f ./ssh_key -t ed25519 -N ""
 
 base64_data=$(base64 --wrap=0 ./ssh_key.pub)
-sed --in-place "s/\(\s\sssh-pubkey:\s\).*$/\1${base64_data}/"
+# sed --in-place "s/\(\s\sssh-pubkey:\s\).*$/\1${base64_data}/" ./jenkins-key.yml
 
-podman kube play jenkins-key.yml
+file_path=/tmp/jenkins-key.yml
+cat <<EOF >${file_path}
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: jenkins-key
+data:
+  ssh-pubkey: ${base64_data}
+EOF
+
+podman kube play ${file_path}
+rm ${file_path}
